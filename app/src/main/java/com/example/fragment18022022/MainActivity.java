7f1c27cdb1c43f216f7ac181d7cae744d187ea1e
Phat <phatdroid94@gmail.com>
2022-06-01 18:33:36 +0700

    delete

--- app/src/main/java/com/example/fragment18022022/MainActivity.java
+++ app/src/main/java/com/example/fragment18022022/MainActivity.java
@@ -10,13 +10,14 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnAddAndroid;
+    Button btnAddAndroid, btnDeleteAndroid;
     FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnAddAndroid = findViewById(R.id.button_add_android_fragment);
+        btnDeleteAndroid = findViewById(R.id.button_delete_android_fragment);
         fragmentManager = getSupportFragmentManager();
 
         btnAddAndroid.setOnClickListener(new View.OnClickListener() {
@@ -24,8 +25,20 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                 AndroidFragment androidFragment = new AndroidFragment();
-                fragmentTransaction.add(R.id.linear_container, androidFragment);
+                fragmentTransaction.add(R.id.linear_container, androidFragment,"android_fragment");
                 fragmentTransaction.commit();
+            }
+        });
+
+        btnDeleteAndroid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AndroidFragment androidFragment = (AndroidFragment) fragmentManager.findFragmentByTag("android_fragment");
+                if (androidFragment != null){
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.remove(androidFragment);
+                    fragmentTransaction.commit();
+                }
             }
         });
     }
