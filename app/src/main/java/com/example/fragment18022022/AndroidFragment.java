6339d84823d84c8058d97fa9ef0745c5c55f55a7
Phat <phatdroid94@gmail.com>
2022-06-01 18:54:03 +0700

    setup

--- app/src/main/java/com/example/fragment18022022/AndroidFragment.java
+++ app/src/main/java/com/example/fragment18022022/AndroidFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -14,6 +15,7 @@ import java.util.Random;
 public class AndroidFragment extends Fragment {
 
     RelativeLayout relativeLayout;
+    TextView tvTitle;
     View view;
     Random random;
     @Override
@@ -22,6 +24,7 @@ public class AndroidFragment extends Fragment {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_android, container, false);
         relativeLayout = view.findViewById(R.id.relative_android);
+        tvTitle = view.findViewById(R.id.text_view_android_fragment);
         random = new Random();
         relativeLayout.setBackgroundColor(Color.rgb(random.nextInt() + 1, random.nextInt() + 1, random.nextInt() + 1));
         return view;
--- app/src/main/java/com/example/fragment18022022/MainActivity.java
+++ app/src/main/java/com/example/fragment18022022/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.fragment18022022;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
@@ -10,17 +11,19 @@ import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnAddAndroid, btnDeleteAndroid;
+    Button btnSendData;
+    EditText edtInput;
     FragmentManager fragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnAddAndroid = findViewById(R.id.button_add_android_fragment);
-        btnDeleteAndroid = findViewById(R.id.button_delete_android_fragment);
+        btnSendData = findViewById(R.id.button_send_data);
+        edtInput = findViewById(R.id.edittext_main);
         fragmentManager = getSupportFragmentManager();
 
-        btnAddAndroid.setOnClickListener(new View.OnClickListener() {
+        btnSendData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -30,26 +33,5 @@ public class MainActivity extends AppCompatActivity {
                 fragmentTransaction.commit();
             }
         });
-
-        btnDeleteAndroid.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AndroidFragment androidFragment = (AndroidFragment) fragmentManager.findFragmentByTag("android_fragment");
-                if (androidFragment != null) {
-                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                    fragmentTransaction.remove(androidFragment);
-                    fragmentTransaction.commit();
-                }
-            }
-        });
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (fragmentManager.getFragments().size() > 0) {
-            fragmentManager.popBackStack();
-        } else {
-            super.onBackPressed();
-        }
     }
 }
