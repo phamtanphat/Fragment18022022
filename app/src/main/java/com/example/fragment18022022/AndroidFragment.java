45cd2ac0cc03628803c73d48ecf3936c97182cb1
Phat <phatdroid94@gmail.com>
2022-06-01 18:25:03 +0700

    random color

--- app/src/main/java/com/example/fragment18022022/AndroidFragment.java
+++ app/src/main/java/com/example/fragment18022022/AndroidFragment.java
@@ -1,18 +1,30 @@
 package com.example.fragment18022022;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import androidx.fragment.app.Fragment;
 
+import java.util.Random;
+
 public class AndroidFragment extends Fragment {
 
+    RelativeLayout relativeLayout;
+    View view;
+    Random random;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_android, container, false);
+        view = inflater.inflate(R.layout.fragment_android, container, false);
+        relativeLayout = view.findViewById(R.id.relative_android);
+        random = new Random();
+        relativeLayout.setBackgroundColor(Color.rgb(random.nextInt() + 1, random.nextInt() + 1, random.nextInt() + 1));
+        return view;
     }
+
 }
--- app/src/main/java/com/example/fragment18022022/MainActivity.java
+++ app/src/main/java/com/example/fragment18022022/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.fragment18022022;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnAddAndroid;
+    FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnAddAndroid = findViewById(R.id.button_add_android_fragment);
+        fragmentManager = getSupportFragmentManager();
+
+        btnAddAndroid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                AndroidFragment androidFragment = new AndroidFragment();
+                fragmentTransaction.add(R.id.linear_container, androidFragment);
+                fragmentTransaction.commit();
+            }
+        });
     }
 }
